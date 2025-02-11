2916b3603a4cf1bc42a3a83aa0691ca1dd5b236e
ivory-red <sept1st@naver.com>
2025-02-11 15:02:27 +0900

    update: root path

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeView />} />
+        <Route path="/portfolio-app" element={<HomeView />} />
       </Routes>
     </BrowserRouter>
   );
